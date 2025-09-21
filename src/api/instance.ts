ddc891a28547c915747972f9a6cf3c1a5b50c0e4
Oleksii Vovnenko <kespa71for@gmail.com>
2025-09-21 12:16:43 +0200

    feat(api): add axios instance

---
+++ src/api/instance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
