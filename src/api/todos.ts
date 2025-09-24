0481af347932b110289fc5da3ea0f1d14f71d640
Oleksii Vovnenko <kespa71for@gmail.com>
2025-09-24 14:43:40 +0200

    chore: create detele function to todo

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,3 +5,8 @@ export async function fetchTodos() {
   const { data } = await api.get<Todo[]>('/todos');
   return data;
 }
+
+export async function deleteTodo(id: number) {
+ const { data } = await api.delete(`/todos/${id}`);
+ return data;
+}
