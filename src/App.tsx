6e2f007888427e2f4f0784fc970fbe39cd958ddc
Oleksii Vovnenko <kespa71for@gmail.com>
2025-09-24 14:28:24 +0200

    feat: add Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './components/Header';
+import Wrapper from './components/Wrapper';
 import TodoList from './features/todos/TodoList';
 
 function App() {
   return (
     <>
       <Header />
-      <TodoList />
+      <Wrapper>
+        <TodoList />
+      </Wrapper>
     </>
   );
 }
