de7bf11d512b1b931777eb0b6df200c2624e529d
Oleksii Vovnenko <kespa71for@gmail.com>
2025-10-09 21:03:58 +0200

    refactor: add  contaiener to header and update styles

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,16 +1,17 @@
+import Container from '../Container';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
+      <Container>
         <div className={styles.wrapper}>
           <Link className={styles.logo} to="/">
             Logo
           </Link>
         </div>
-      </div>
+      </Container>
     </header>
   );
 };
