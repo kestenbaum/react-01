0cb29f7d3234107c62e8f5492f9f83ab6ae516f2
Oleksii Vovnenko <kespa71for@gmail.com>
2025-10-16 20:40:16 +0200

    feat: add custom btn

---
+++ src/widgets/Button/index.tsx
@@ -0,0 +1,17 @@
+import { type ButtonHTMLAttributes, type FC } from 'react';
+
+import styles from '../Button/Button.module.css';
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string | number;
+}
+
+const Button: FC<IButton> = ({ children, ...rest }) => {
+  return (
+    <button className={styles.btn} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
