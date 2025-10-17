e38a9c999e915084ebab689a4216d69ce98bc3ae
Oleksii Vovnenko <kespa71for@gmail.com>
2025-10-17 10:51:02 +0200

    feat: add search to todos

--- src/features/todo/TodoList.tsx
+++ src/features/todo/TodoList.tsx
@@ -3,33 +3,51 @@ import { useEffect } from 'react';
 import type { AppDispatch } from '../../app/store/index';
 import {
   getTodos,
-  selectedTodos,
+  selectedFiltersTodos,
   selectedTodosError,
   selectedTodosPage,
   selectedTodosStatus,
+  selectedTodosTotal,
+  setFilter,
 } from '../../app/store/slice/todoSlice';
-import Button from '../../widgets/Button';
+import { shouldShowPagination } from '../../shared/lib/shouldShowPagination';
+import Input from '../../widgets/Input';
 import Pagination from '../../widgets/Pagination';
 import ToolsBar from '../../widgets/ToolsBar';
 import { useDispatch, useSelector } from 'react-redux';
 
 const TodoList = () => {
+  const limit = 10;
   const dispatch = useDispatch<AppDispatch>();
-  const todos = useSelector(selectedTodos);
+  const todos = useSelector(selectedFiltersTodos);
   const status = useSelector(selectedTodosStatus);
   const errors = useSelector(selectedTodosError);
   const page = useSelector(selectedTodosPage);
+  const total = useSelector(selectedTodosTotal);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault;
+    dispatch(setFilter(event.target.value));
+  };
 
   useEffect(() => {
-    dispatch(getTodos({ page, limit: 10 }));
+    dispatch(getTodos({ page, limit }));
   }, [dispatch, page]);
 
   if (status == 'loading') return <div>Loading...</div>;
   if (errors) return <div>${'Error: ' + errors}</div>;
 
   return (
-    <div>
-      <ToolsBar children={<Button>Filters</Button>} />
+    <section>
+      <ToolsBar
+        children={
+          <Input
+            type="text"
+            placeholder="Search Todo..."
+            onChange={handleSearch}
+          />
+        }
+      />
       {todos.map((todo) => (
         <div
           key={todo.id}
@@ -43,8 +61,8 @@ const TodoList = () => {
         </div>
       ))}
 
-      <Pagination />
-    </div>
+      {shouldShowPagination(total, limit, todos.length) && <Pagination />}
+    </section>
   );
 };
 
